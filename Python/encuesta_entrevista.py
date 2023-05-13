580e1d29dc178cbf7773b4f0149e65ccaff6132e
rubenglezortiz <rubgon08@ucm.es>
2023-05-13 21:25:52 +0200

    cambios últimas gráficas encuesta_entrevista
    Co-Authored-By: AlvaroCuerva <alcuer01@ucm.es>
    Co-Authored-By: Rodrigo Tobar <62950284+rtobar01@users.noreply.github.com>

--- Python/encuesta_entrevista.py
+++ Python/encuesta_entrevista.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from collections import Counter
 array_build_A = []
 array_build_B = []
-abundanciaMunicion = []
+abundancia_municion = []
 balances_armas_build_A = []
 balances_armas_build_B = []
 intuicion_apuntado = []
@@ -19,6 +19,8 @@ dificultad_build_A = []
 dificultad_build_B = []
 uso_apuntado_build_A = []
 uso_apuntado_build_B = []
+COLORS = ['#ef476f', '#00b4d8', '#f2e8cf', '#06d6a0', '#118ab2', '#ffcdb2']
+DIFFICULTY_COLORS = ['#ebb9ff', '#a06bff','#7c3aed', '#ff52b4', '#ff2052', '#9b1b30']
 
 def initData():
     with open('Datos/encuesta_entrevista.csv', newline='') as archivo_csv:
@@ -35,7 +37,7 @@ def initData():
                 array_build_B.append(fila_sin_tildes)
 
     for i in array_build_A:
-        abundanciaMunicion.append(int(i[0]))
+        abundancia_municion.append(int(i[0]))
         balances_armas_build_A.append(i[1])
         intuicion_apuntado.append(int(i[2]))
         frenetismo_apuntado.append(int(i[3]))
@@ -46,7 +48,7 @@ def initData():
         uso_apuntado_build_A.append(i[16])
 
     for i in array_build_B:
-        abundanciaMunicion.append(int(i[0]))
+        abundancia_municion.append(int(i[0]))
         balances_armas_build_B.append(i[1])
         intuicion_apuntado.append(int(i[2]))
         frenetismo_apuntado.append(int(i[3]))
@@ -57,111 +59,66 @@ def initData():
         uso_apuntado_build_B.append(i[16])
 
 def grafica_municion():
-    # Crear la figura y los ejes
-    frecuencias=dict(Counter(abundanciaMunicion))
-    frecuenas_ordenadas = sorted(frecuencias.keys())
-    etiquetas = []
-    for valor in frecuenas_ordenadas:
-        if valor == 0:
-            etiquetas.append("0 muy escasa")
-        elif valor == 1:
-            etiquetas.append("1 bastante escasa")
-        elif valor == 2:
-            etiquetas.append("2 escasa")
-        elif valor == 3:
-            etiquetas.append("3 abundante")
-        elif valor == 4:
-            etiquetas.append("4 bastante abundante")
-        else:
-            etiquetas.append("5 muy abundante")
-    fig, ax = plt.subplots(2)
-    # Crear el gráfico de pastel
-    ax[0].set_title('Cantidad de munición a lo largo de los niveles')
-    ax[0].pie(frecuencias.values(), labels=etiquetas,autopct='%1.1f%%', startangle=90)
+    # Inicializar etiquetas y alturas de la gráfica
+    frecuencias=[0,0,0,0,0,0]
+    for i in abundancia_municion:
+        frecuencias[i] = frecuencias[i]+1
+    etiquetas = ["Muy escasa", "Bastante escasa", "Escasa", "Abundante", "Bastante abundante", "Muy abundante"]
+    
     # Crear el gráfico de barras
-    ax[1].bar(etiquetas,  height=frecuencias.values())
-    ax[1].set_xlabel('Abundancia')
-    ax[1].set_ylabel('Número de personas')
+    plt.title('Cantidad de munición a lo largo de los niveles')
+    plt.bar(etiquetas, height=frecuencias, color=DIFFICULTY_COLORS)
     plt.show()
 
 def grafica_balance_armas():
-    #Guardamos en la lista las respuestas
-    # Crear la figura y los ejes
-    frecuencias_build_A=dict(Counter(balances_armas_build_A))
-    frecuencias_build_B=dict(Counter(balances_armas_build_B))
-    frecuencias_build_A_ordenadas = sorted(frecuencias_build_A.keys())
-    frecuencias_build_B_ordenadas = sorted(frecuencias_build_B.keys())
+    # Inicializar etiquetas y frecuencias de la gráfica
+    frecuencias_build_A=[0,0,0]
+    frecuencias_build_B=[0,0,0]
+    for i in balances_armas_build_A:
+        if(i == "Sí"):
+            frecuencias_build_A[0] = frecuencias_build_A[0]+1
+        elif(i == "No"):
+            frecuencias_build_A[1] = frecuencias_build_A[1]+1
+        else: frecuencias_build_A[2] = frecuencias_build_A[2]+1
+    
+    for i in balances_armas_build_B:
+        if(i == "Sí"):
+            frecuencias_build_B[0] = frecuencias_build_B[0]+1
+        elif(i == "No"):
+            frecuencias_build_B[1] = frecuencias_build_B[1]+1
+        else: frecuencias_build_B[2] = frecuencias_build_B[2]+1
+    etiquetas = ["Sí", "No", "Depende de las circustancias"]
 
-    etiquetas_build_A = []
-    etiquetas_build_B = []
-    for valor in frecuencias_build_A_ordenadas:
-        if valor == "Sí":
-            etiquetas_build_A.append("Sí")
-        elif valor == "No":
-            etiquetas_build_A.append("No")
-        else:
-            etiquetas_build_A.append("Depende de las circustancias")
-
-    for valor in frecuencias_build_B_ordenadas:
-        if valor == "Sí":
-            etiquetas_build_B.append("Sí")
-        elif valor == "No":
-            etiquetas_build_B.append("No")
-        else:
-            etiquetas_build_B.append("Depende de las circustancias")
-
+    # Crear el gráfico pie
     fig, ax = plt.subplots(2)
-
     ax[0].set_title('¿Consideras que el cuchillo está balanceado en comparación a la pistola? - BUILD A')
-    ax[0].pie(frecuencias_build_A.values(), labels=etiquetas_build_A,autopct='%1.1f%%', startangle=90)
+    ax[0].pie(frecuencias_build_A, labels=etiquetas,autopct='%1.1f%%', startangle=90, colors=COLORS)
 
     ax[1].set_title('¿Consideras que el cuchillo está balanceado en comparación a la pistola? - BUILD B')
-    ax[1].pie(frecuencias_build_B.values(), labels=etiquetas_build_B,autopct='%1.1f%%', startangle=90)
+    ax[1].pie(frecuencias_build_B, labels=etiquetas,autopct='%1.1f%%', startangle=90, colors=COLORS)
     plt.show()
 
 def grafica_apuntado_pistola():
-    # Crear la figura y los ejes
-    frecuencias_intuicion=dict(Counter(intuicion_apuntado))
-    frecuencias_frenetismo=dict(Counter(frenetismo_apuntado))
-    frecuencias_intuicion_ordenadas = sorted(frecuencias_intuicion.keys())
-    frecuencias_frenetismo_ordenadas = sorted(frecuencias_frenetismo.keys())
-    etiquetas_intuicion = []
-    etiquetas_frenetismo = []
-    for valor in frecuencias_intuicion_ordenadas:
-        if valor == 0:
-            etiquetas_intuicion.append("0 nada intuitivo")
-        elif valor == 1:
-            etiquetas_intuicion.append("1 muy poco intuitivo")
-        elif valor == 2:
-            etiquetas_intuicion.append("2 poco intuitivo")
-        elif valor == 3:
-            etiquetas_intuicion.append("3 algo intuitivo")
-        elif valor == 4:
-            etiquetas_intuicion.append("4 bastante intuitivo")
-        else:
-            etiquetas_intuicion.append("5 muy intuitivo")
-    for valor in frecuencias_frenetismo_ordenadas:
-        if valor == 0:
-            etiquetas_intuicion.append("0 nada frenético")
-        elif valor == 1:
-            etiquetas_frenetismo.append("1 muy poco frenético")
-        elif valor == 2:
-            etiquetas_frenetismo.append("2 poco frenético")
-        elif valor == 3:
-            etiquetas_frenetismo.append("3 algo frenético")
-        elif valor == 4:
-            etiquetas_frenetismo.append("4 bastante frenético")
-        else:
-            etiquetas_frenetismo.append("5 muy frenético")
+    # Inicializar etiquetas y alturas de la gráfica
+    frecuencias_intuicion=[0,0,0,0,0,0]
+    frecuencias_frenetismo=[0,0,0,0,0,0]
+    for i in intuicion_apuntado:
+        frecuencias_intuicion[i] = frecuencias_intuicion[i] + 1
+    for i in frenetismo_apuntado:
+        frecuencias_frenetismo[i] = frecuencias_frenetismo[i] + 1
+    etiquetas_intuicion = ["Nada intuitivo", "Muy poco intuitivo", "Poco intuitivo", "Algo intuitivo", "Bastante intuitivo", "Muy intuitivo"]
+    etiquetas_frentismo = ["Nada frenético", "Muy poco frenético", "Poco frenético", "Algo frenético", "Bastante frenético", "Muy frenético"]
 
+    # Crear el gráfico de barras
     fig, ax = plt.subplots(2)
+    fig.subplots_adjust(hspace=0.8)
     ax[0].set_title('Intuición apuntado pistola')
-    ax[0].bar(etiquetas_intuicion,  height=frecuencias_intuicion.values())
-    ax[0].set_xlabel('Intuición')
+    ax[0].bar(etiquetas_intuicion,  height=frecuencias_intuicion, color=DIFFICULTY_COLORS)
+    ax[0].set_xticklabels(etiquetas_intuicion, rotation=45, fontsize=8)
     ax[0].set_ylabel('Número de personas')
     ax[1].set_title('Frenetismo apuntado pistola')
-    ax[1].bar(etiquetas_frenetismo,  height=frecuencias_frenetismo.values())
-    ax[1].set_xlabel('Frenetismo')
+    ax[1].bar(etiquetas_frentismo,  height=frecuencias_frenetismo, color=DIFFICULTY_COLORS)
+    ax[1].set_xticklabels(etiquetas_frentismo, rotation=45, fontsize=8)
     ax[1].set_ylabel('Número de personas')
     plt.show()
 
@@ -176,108 +133,96 @@ def grafica_dificultad_enemigos():
         frecuencias_build_B[d] = frecuencias_build_B[d]+1
     
     ax[0].set_title('¿Cuál ha sido la dificultad para eliminar enemigos? - BUILD A')
-    ax[0].bar(etiquetas,  height=frecuencias_build_A)
+    ax[0].bar(etiquetas,  height=frecuencias_build_A, color = DIFFICULTY_COLORS)
     ax[1].set_title('¿Cuál ha sido la dificultad para eliminar enemigos? - BUILD B')
-    ax[1].bar(etiquetas,  height=frecuencias_build_B)
+    ax[1].bar(etiquetas,  height=frecuencias_build_B, color = DIFFICULTY_COLORS)
     plt.show()
 
 def grafica_economizacion_municion():
+    # Inicializar etiquetas y frecuencias de la gráfica
+    frecuencias_build_A=[0,0,0]
+    frecuencias_build_B=[0,0,0]
+    for i in municion_build_A:
+        if(i == "Sí"):
+            frecuencias_build_A[0] = frecuencias_build_A[0]+1
+        elif(i == "No"):
+            frecuencias_build_A[1] = frecuencias_build_A[1]+1
+        else: frecuencias_build_A[2] = frecuencias_build_A[2]+1
+    
+    for i in municion_build_B:
+        if(i == "Sí"):
+            frecuencias_build_B[0] = frecuencias_build_B[0]+1
+        elif(i == "No"):
+            frecuencias_build_B[1] = frecuencias_build_B[1]+1
+        else: frecuencias_build_B[2] = frecuencias_build_B[2]+1    
+    etiquetas = ["Sí", "No", "Uso nulo de la pistola"]
 
-    # Crear la figura y los ejes
-    frecuencias_build_A=dict(Counter(municion_build_A))
-    frecuencias_build_B=dict(Counter(municion_build_B))
-    etiquetas_build_A = []
-    etiquetas_build_B = []
-    frecuancias_build_A_ordenadas = sorted(frecuencias_build_A.keys())
-    frecuancias_build_B_ordenadas = sorted(frecuencias_build_B.keys())
-    for valor in frecuancias_build_A_ordenadas:
-        if valor == "Sí":
-            etiquetas_build_A.append("Sí")
-        elif valor == "No":
-            etiquetas_build_A.append("No")
-        else:
-            etiquetas_build_A.append("Uso nulo de la pistola")
-
-    for valor in frecuancias_build_B_ordenadas:
-        if valor == "Sí":
-            etiquetas_build_B.append("Sí")
-        elif valor == "No":
-            etiquetas_build_B.append("No")
-        else:
-            etiquetas_build_B.append("Uso nulo de la pistola")
-
+    # Crear el gráfico pie
     fig, ax = plt.subplots(2)
-
     ax[0].set_title('¿El jugador ha economizado la munición que tenía? - BUILD A')
-    ax[0].pie(frecuencias_build_A.values(), labels=etiquetas_build_A,autopct='%1.1f%%', startangle=90)
+    ax[0].pie(frecuencias_build_A, labels=etiquetas, autopct='%1.1f%%', startangle=90, colors = COLORS)
 
     ax[1].set_title('¿El jugador ha economizado la munición que tenía? - BUILD B')
-    ax[1].pie(frecuencias_build_B.values(), labels=etiquetas_build_B,autopct='%1.1f%%', startangle=90)
+    ax[1].pie(frecuencias_build_B, labels=etiquetas, autopct='%1.1f%%', startangle=90, colors = COLORS)
     plt.show()
 
 def grafica_frustracion_laser():
-    # Crear la figura y los ejes
-    frecuencias_build_A=dict(Counter(frustracion_laser_build_A))
-    frecuencias_build_B=dict(Counter(frustracion_laser_build_B))
-    etiquetas_build_A = []
-    etiquetas_build_B = []
-    frecuancias_build_A_ordenadas = sorted(frecuencias_build_A.keys())
-    frecuancias_build_B_ordenadas = sorted(frecuencias_build_B.keys())
-    for valor in frecuancias_build_A_ordenadas:
-        if valor == "Sí":
-            etiquetas_build_A.append("Sí")
-        else:
-            etiquetas_build_A.append("No")
-
-    for valor in frecuancias_build_B_ordenadas:
-        if valor == "Sí":
-            etiquetas_build_B.append("Sí")
-        else:
-            etiquetas_build_B.append("No")
-
+    # Inicializar etiquetas y frecuencias de la gráfica
+    frecuencias_build_A=[0,0]
+    frecuencias_build_B=[0,0]
+    for i in frustracion_laser_build_A:
+        if(i == "Sí"):
+            frecuencias_build_A[0] = frecuencias_build_A[0]+1
+        elif(i == "No"):
+            frecuencias_build_A[1] = frecuencias_build_A[1]+1
+        else: frecuencias_build_A[2] = frecuencias_build_A[2]+1
+    
+    for i in frustracion_laser_build_B:
+        if(i == "Sí"):
+            frecuencias_build_B[0] = frecuencias_build_B[0]+1
+        elif(i == "No"):
+            frecuencias_build_B[1] = frecuencias_build_B[1]+1
+        else: frecuencias_build_B[2] = frecuencias_build_B[2]+1    
+    etiquetas = ["Sí", "No"]
+   
     fig, ax = plt.subplots(2)
 
     ax[0].set_title('¿Se ha frustrado el jugador por los láseres? - BUILD A')
-    ax[0].pie(frecuencias_build_A.values(), labels=etiquetas_build_A,autopct='%1.1f%%', startangle=90)
+    ax[0].pie(frecuencias_build_A, labels=etiquetas,autopct='%1.1f%%', startangle=90, colors = COLORS)
 
     ax[1].set_title('¿Se ha frustrado el jugador por los láseres? - BUILD B')
-    ax[1].pie(frecuencias_build_B.values(), labels=etiquetas_build_B,autopct='%1.1f%%', startangle=90)
+    ax[1].pie(frecuencias_build_B, labels=etiquetas, autopct='%1.1f%%', startangle=90, colors = COLORS)
     plt.show()
 
 def grafica_dificultad_enemigos_laseres():
-    # Crear la figura y los ejes
-    frecuencias_build_A=dict(Counter(dificultad_build_A))
-    frecuencias_build_B=dict(Counter(dificultad_build_B))
-    frecuancias_build_A_ordenadas = sorted(frecuencias_build_A.keys())
-    frecuancias_build_B_ordenadas = sorted(frecuencias_build_B.keys())
-    etiquetas_build_A = []
-    etiquetas_build_B = []
-    for valor in frecuancias_build_A_ordenadas:
-        if valor == "Enemigos":
-            etiquetas_build_A.append("Enemigos")
-        else:
-            etiquetas_build_A.append("Láseres")
+    # Inicializar etiquetas y frecuencias de la gráfica
+    frecuencias_build_A=[0,0]
+    frecuencias_build_B=[0,0]
+    for i in dificultad_build_A:
+        if(i == "Enemigos"):
+            frecuencias_build_A[0] = frecuencias_build_A[0]+1
+        else: frecuencias_build_A[1] = frecuencias_build_A[1]+1
+    
+    for i in dificultad_build_B:
+        if(i == "Enemigos"):
+            frecuencias_build_B[0] = frecuencias_build_B[0]+1
+        else: frecuencias_build_B[1] = frecuencias_build_B[1]+1    
+    etiquetas = ["Enemigos", "Láseres"]
 
-    for valor in frecuancias_build_B_ordenadas:
-        if valor == "Enemigos":
-            etiquetas_build_B.append("Enemigos")
-        else:
-            etiquetas_build_B.append("Láseres")
-
+    # Crear el gráfico pie
     fig, ax = plt.subplots(2)
-
     ax[0].set_title('¿Por qué ha sentido el jugador más peligro? - BUILD A')
-    ax[0].pie(frecuencias_build_A.values(), labels=etiquetas_build_A,autopct='%1.1f%%', startangle=90)
+    ax[0].pie(frecuencias_build_A, labels=etiquetas, autopct='%1.1f%%', startangle=90, colors = COLORS)
 
     ax[1].set_title('¿Por qué ha sentido el jugador más peligro? - BUILD B')
-    ax[1].pie(frecuencias_build_B.values(), labels=etiquetas_build_B,autopct='%1.1f%%', startangle=90)
+    ax[1].pie(frecuencias_build_B, labels=etiquetas, autopct='%1.1f%%', startangle=90, colors = COLORS)
     plt.show()
 
 initData()
 # grafica_municion()
 # grafica_balance_armas()
 # grafica_apuntado_pistola()
-grafica_dificultad_enemigos()
+# grafica_dificultad_enemigos()
 # grafica_economizacion_municion()
 # grafica_frustracion_laser()
 # grafica_dificultad_enemigos_laseres()
